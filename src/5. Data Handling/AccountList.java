7d846d4a95fb61d8168da1e619c590b8fe6267f4
Liam Skinner <me@liamskinner.co.uk>
2023-11-10 22:43:12 +0000

    Data(File Writing): export records to text file

--- src/5. Data Handling/AccountList.java
+++ src/5. Data Handling/AccountList.java
@@ -1,3 +1,6 @@
+/* Import Required Modules & Packages */
+import java.io.FileWriter;
+
 public class AccountList {
 
     /* Create an Empty Array with 20 Spaces */
@@ -12,7 +15,11 @@ public class AccountList {
             allAccounts[totalRecords] = record;
             totalRecords = totalRecords + 1;
 
-            System.out.println("Record Added.");
+            /* Export to the CSV */
+            boolean status = exportAccount(record);
+            String response = (status == true) ? " and Exported" : " but not Exported";
+
+            System.out.println("Record Added%s.".formatted(response));
         }
         else { /* The Array has been Filled */
             System.out.println("Database is full...");
@@ -27,10 +34,43 @@ public class AccountList {
             System.out.println(myRecord.toString());
         };
     };
+    /* Export all Accounts to a CSV File */
+    public void exportAllAccounts() {
 
+        try {
+            FileWriter myFile = new FileWriter("MyData.txt", false);
+
+            for (int index = 0; index < totalRecords; index++) {
+                BankAccount myRecord = allAccounts[index];
+                myFile.write(myRecord.toString() + "\r\n");
+            };
+
+            myFile.close();
+            System.out.println("Successfully exported all accounts.");
+
+        } catch (Exception Exc) {
+            System.out.println("Woah. An error occurred.");
+        };
+    };
+
+    
     /* Search for a Specific Element and use .toString() */
     public void outputAccount(int index) {
         BankAccount myRecord = allAccounts[index];
         System.out.println(myRecord.toString());
+    };
+    /* Export a Specific Account to a CSV File */
+    public boolean exportAccount(BankAccount record) {
+        try {
+            FileWriter myFile = new FileWriter("MyData.txt", true);
+
+            myFile.write(record.toString() + "\r\n");
+            myFile.close();
+
+        } catch (Exception Exc) {
+            return false;
+        };
+
+        return true;
     };
 };
--- src/5. Data Handling/BankAccount.java
+++ src/5. Data Handling/BankAccount.java
@@ -5,7 +5,7 @@ public class BankAccount {
 
     /* Declare Global Variables */
     public String accountNumber;
-    public String sortCode;
+    public String sortCode ;
     private int balance = 0;
     public int maxOverdraft = -1000;
     public char userType = 'C';
@@ -14,7 +14,7 @@ public class BankAccount {
         /* Reset These Values */
         accountNumber = "";
         sortCode = "";
-
+        
         Scanner inputScanner = new Scanner(System.in);
 
         /* Enter AccountNumber as 8 Digits: 12345678 */
@@ -53,19 +53,19 @@ public class BankAccount {
         balance = balance + amount;
         System.out.println("Your new balance is £" + balance + ".00");
     };
-
+    
     /* Subtract an Amount from the Balance */
     public void makeWithdrawal(int amount) {
         balance = balance - amount;
-
-        /* Compare with the MaxOverdraft Limit */
+        
+        /* Compare against the MaxOverdraft Limit */
         if (balance < maxOverdraft) {
             System.out.println("You cannot withdraw £%s and go past the £%s overdraft...".formatted(
                 amount, maxOverdraft
             ));
             balance = balance + amount;
             return;
-        };
+        }
         System.out.println("Your new balance is £" + balance + ".00");
     };
 
