1f48438fd808145facbf01d28466354bbc2f345f
Liam S <me@liamskinner.co.uk>
2024-03-12 09:49:34 +0000

    chore(DataHandling): refactored task book

--- src/5. Data Handling/AccountList.java
+++ src/5. Data Handling/AccountList.java
@@ -1,174 +1,94 @@
-/* Import Required Modules & Packages */
-import java.io.*;
-
-public class AccountList {
-
-    /* Create an Empty Array with 100 Spaces */
-    BankAccount[] allAccounts = new BankAccount[100];
-    int totalRecords = 0;
-
-    /* Add a BankAccount to the Array */
-    public void addAccountToList(BankAccount record) {
-
-        /* Is there Space in the Array */
-        if (totalRecords < allAccounts.length) {
-            allAccounts[totalRecords] = record;
-            totalRecords = totalRecords + 1;
-
-            /* Export to the CSV */
-            boolean status = exportAccount(record);
-            String response = (status == true) ? "and Exported" : "but not Exported";
-
-            System.out.println("Record Added %s.".formatted(response));
-        }
-        else { /* The Array has been Filled */
-            System.out.println("Database is full...");
-        };
-    };
-
-    /* Loop through the Array and run .toString() on every Element */
-    public void outputAllAccounts() {
-
-        for (int index = 0; index < totalRecords; index++) {
-            BankAccount myRecord = allAccounts[index];
-            System.out.println(myRecord.toString());
-        };
-    };
-    /* Export all Accounts to a CSV File */
-    public void exportAllAccounts() {
-
-        try {
-            FileWriter myFile = new FileWriter("MyData.txt", false);
-
-            for (int index = 0; index < totalRecords; index++) {
-                BankAccount myRecord = allAccounts[index];
-                myFile.write(myRecord.toString() + "\r\n");
-            };
-
-            myFile.close();
-            System.out.println("Successfully exported all accounts.");
-
-        } catch (Exception Exc) {
-            System.out.println("Woah. An error occurred.");
-        };
-    };
-
-
-    /* Search for a Specific Element and use .toString() */
-    public void outputAccount(int index) {
-        BankAccount myRecord = allAccounts[index];
-        System.out.println(myRecord.toString());
-    }; 
-    /* Export a Specific Account to a CSV File */
-    public boolean exportAccount(BankAccount record) {
-        String data = encrypt( record.toString() );
-
-        try {
-            FileWriter myFile = new FileWriter("MyData.txt", true);
-            myFile.write(data + "\r\n");
-            myFile.close();
-        } catch (Exception Exc) { return false; };
-
-        return true;
-    };
-
-
-    /* Import Accounts from a CSV File */
-    public void importAllAccounts() {
-        /* Reset the data within the Array */
-        allAccounts = new BankAccount[100];
-        totalRecords = 0;
-
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("RecordsOutputCommas.txt"));
-
-            for (String line = br.readLine(); line != null; line = br.readLine()) {
-
-                /* Split the CSVs */
-                String stringData = decrypt( line );
-                String[] data = stringData.split(",");
-
-                BankAccount newAccount = new BankAccount();
-                newAccount.accountNumber = data[0];
-                newAccount.sortCode = data[1];
-
-                newAccount.setBalance( Integer.parseInt( data[2] ) );
-                newAccount.maxOverdraft = Integer.parseInt( data[3] );
-
-                newAccount.userType = data[4].charAt(0);
-
-                /* Add the Account to the List */
-                addAccountToList(newAccount);
-            };
-            br.close();
-
-        } catch (Exception exc) {
-            System.out.println("File Reading Error");
-            exc.printStackTrace();
-        };
-    };
-
-    /* Search for an Account with a SearchQuery */
-    public int searchByAccountNumber(String searchQuery) { 
-        for (int index = 0; index < totalRecords; index++) {
-            BankAccount baTemp = allAccounts[index];
-
-            if (baTemp.accountNumber.equals(searchQuery)) {
-                return index;
-            };
-        };
-        return -1;
-    };
-    public int searchBySortCode(String searchQuery) { 
-        for (int index = 0; index < totalRecords; index++) {
-            BankAccount baTemp = allAccounts[index];
-
-            if (baTemp.sortCode.equals(searchQuery)) {
-                return index;
-            };
-        };
-        return -1;
-    };
-
-    /* Sort the Accounts by Balance */
-    public void sortByBalance() {
-        /* Loop through all the Accounts */
-        for (int outer = 0; outer < totalRecords; outer++) {
-            for (int inner = 0; inner < (totalRecords - 1); inner++) {
-
-                /* Fetch Account Balances */
-                int balanceOne = allAccounts[inner].getBalance();
-                int balanceTwo = allAccounts[inner + 1].getBalance();
-
-                /* Swap the values if "Bal < Bal+1" */
-                /* Largest => ... => Smallest */
-                if (balanceOne < balanceTwo) {
-                    BankAccount swappingValue = allAccounts[inner];
-                    allAccounts[inner] = allAccounts[inner + 1];
-                    allAccounts[inner + 1] = swappingValue;
-                };
-            };
-        };
-    };
-
-
-    /* Encrypt and Decrypt data from TXT Files */
-    public String encrypt(String data) {
-        String encryptedData = "";
-
-        for (int index = 0; index < data.length(); index++) {
-            int ascii = data.charAt(index) + 48; // Caeser Shift +48
-            encryptedData = encryptedData + (char)ascii;
-        };
-        return encryptedData;
-    };
-    public String decrypt(String encryptedData) {
-        String data = "";
-
-        for (int index = 0; index < encryptedData.length(); index++) {
-            int ascii = encryptedData.charAt(index) - 48; // Caeser Shift -48
-            data = data + (char)ascii;
-        };
-        return data;
-    };
-};
+/**
+ * This class conforms to the regulations set forth within the
+ * "Bank Project" EBook's Day 2 Task.
+ * 
+ * @author Liam Skinner
+ * 
+ * Credit: Winstanley College
+ *         for the creation of this task.
+**/
+public class AccountList
+{
+	BankAccount[] allAccounts = new BankAccount[20];
+	int currentIndex = 0;
+	
+	/**
+	 * Polymorphism
+	 * 
+	 * This method will interate over every non-null value of the
+	 * allAccounts array, and print the toString() value of each.
+	 */
+	public void displayAccounts()
+	{
+		// Interates over all non-null elements
+		for (int index = 0; index < currentIndex; index++)
+		{
+			String stringAccount = allAccounts[index].toString();
+			System.out.println(stringAccount);
+		}
+	}
+	
+	/**
+	 * Polymorphism
+	 * 
+	 * This method will display the toString() value
+	 * of the specific account provided.
+	 * 
+	 * @param   indexPoint    The account position to display
+	 */
+	public void displayAccounts(int indexPoint)
+	{
+		String stringAccount = allAccounts[indexPoint].toString();
+		System.out.println(stringAccount);
+	}
+	
+	
+	/**
+	 * This method will take BankAccount parameter, and add it to the
+	 * next non-null element of the array.
+	 * 
+	 * @param   newAccount    The BankAccount to append to the array
+	 */
+	public void addAccountToList(BankAccount newAccount)
+	{
+		// Ensures there is still non-null elements in the array
+		if (currentIndex >= allAccounts.length - 1)
+		{
+			System.out.println("addAccountToList: unsuccessful - allAccounts array is full.");
+			return;
+		}
+		
+		// Ensures no account has matching accountNumber & sortCode
+		String accountInformation = getAccountInformation(newAccount);
+		
+		for (int index = 0; index < currentIndex; index++)
+		{
+			BankAccount otherAccount = allAccounts[index];
+			String otherAccountInformation = getAccountInformation(otherAccount);
+			
+			if (otherAccountInformation.equals(accountInformation))
+			{
+				System.out.println("addAccountToList: unsuccessful - replication of account information.");
+				return;
+			}
+		}
+				
+		allAccounts[currentIndex] = newAccount;
+		currentIndex++;
+	}
+	
+	/**
+	 * This method used within the addAccountToList() as validation,
+	 * it will return the string combination of the AccountNumber and
+	 * SortCode to allow for unique checking.
+	 * 
+	 * @return     CSV of AccountNumber and SortCode
+	 */
+	public String getAccountInformation(BankAccount theAccount)
+	{
+		return "%s,%s".formatted(
+			theAccount.getAccountNumber(),
+			theAccount.getSortCode()
+		);
+	}
+}
--- src/5. Data Handling/BankAccount.java
+++ src/5. Data Handling/BankAccount.java
@@ -1,81 +1,179 @@
-/* Import Required Modules & Packages */
 import java.util.Scanner;
 
-public class BankAccount {
+/**
+ * This class conforms to the regulations set forth within the
+ * "Bank Project" EBook's Day 1 Task.
+ * 
+ * @author Liam Skinner
+ * 
+ * Credit: Winstanley College
+ *         for the creation of this task.
+**/
+public class BankAccount
+{
+	private String accountNumber;
+	private String sortCode;
+	
+	private int balance;
+	private int maxOverdraft;
+	
+	private char userType;
+	
 
-    /* Declare Global Variables */
-    public String accountNumber;
-    public String sortCode;
-    private int balance = 0;
-    public int maxOverdraft = -1000;
-    public char userType = 'C';
+	/**
+	 * GETTER
+	 * 
+	 * This method will return the current value of the balance variable
+	 * 
+	 * @return  Integer value of the balance
+	 */
+	public int getBalance()
+	{
+		return balance;
+	}
+	/**
+	 * GETTER
+	 * 
+	 * This method will return the current value of the accountNumber variable
+	 * 
+	 * @return  String value of the account number
+	 */
+	public String getAccountNumber()
+	{
+		return accountNumber;
+	}
+	/**
+	 * GETTER
+	 * 
+	 * This method will return the current value of the sortCode variable
+	 * 
+	 * @return  String value of the sortCode
+	 */
+	public String getSortCode()
+	{
+		return sortCode;
+	}
+	
+	/**
+	 * SETTER
+	 * 
+	 * This method will set the current value of the balance variable
+	 * 
+	 * @param     amount   New balance amount
+	 * @return             Setter Successful Flag
+	 */
+	public boolean setBalance(int amount)
+	{
+		if (amount < maxOverdraft)
+		{
+			System.out.println("setBalance: unsuccessful ~ cannot exceed maxoverdraft");
+			return false;
+		}
+		
+		balance = amount;
+		System.out.println("setBalance: balance is now £" + balance);
 
-    public void setAllValues() {
-        /* Reset These Values */
-        accountNumber = "";
-        sortCode = "";
+		return true;
+	}
 
-        Scanner inputScanner = new Scanner(System.in);
 
-        /* Enter AccountNumber as 8 Digits: 12345678 */
-        while (accountNumber.matches("\\d{8}") == false) {
-            System.out.print("Enter Account Number: ");
-            accountNumber = inputScanner.nextLine();
-        };
+	/**
+	 * DEBUG METHOD
+	 * 
+	 * This method will take an integer parameter, and
+	 * adds that amount onto the current user balance.
+	 * 
+	 * @param   amount    The amount to increase the balance by
+	 * @return            Deposit Successful Flag
+	 */
+	public boolean debugMakeDeposit(int amount)
+	{
+		if (amount <= 0)
+		{
+			System.out.println("debugMakeDeposit: unsuccessful ~ invalid amount provided");
+			return false;
+		}
+		
+		balance = balance + amount;
+		System.out.println("debugMakeDeposit: balance is now £" + balance);
 
-        /* Enter SortCode as 6 Digits: 123456 */
-        while (sortCode.matches("\\d{6}") == false) {
-            System.out.print("Enter SortCode: ");
-            sortCode = inputScanner.nextLine();
-        };
+		return true;
+	}
+	
+	/**
+	 * DEBUG METHOD
+	 * 
+	 * This method will take an integer parameter, and
+	 * substracts that amount from the current user balance.
+	 * 
+	 * @param   amount    The amount to decrease the balance by
+	 * @return            Withdrawal Successful Flag
+	 */
+	public boolean debugMakeWithdrawal(int amount)
+	{
+		if (amount <= 0)
+		{
+			System.out.println("debugMakeWithdrawal: unsuccessful ~ invalid amount provided");
+			return false;
+		}
+		
+		if (amount > (balance - maxOverdraft))
+		{
+			System.out.println("debugMakeWithdrawal: unsuccessful ~ cannot exceed maxoverdraft");
+			return false;
+		}
+		
+		balance = balance - amount;
+		System.out.println("debugMakeWithdrawal: balance is now £" + balance);
+		return true;
+	}
+	
+	
+	/**
+	 * DEBUG METHOD
+	 * 
+	 * This method acts as a constructor and sets the
+	 * required values within the global variables. Through
+	 * user inputs, and predefined options.
+	 */
+	public void debugSetAllValues()
+	{
+		Scanner inputScanner = new Scanner(System.in);
+		
+		accountNumber = "";
+		while (accountNumber.matches("\\d{8}") == false)
+		{
+			System.out.print("Enter Account Number: ");
+			accountNumber = inputScanner.nextLine();
+		}
 
-        inputScanner.close();
-
-        /* Automatically Set These Values */
-        balance = 0;
-        maxOverdraft = -1000;
-        userType = 'C';
-    };
-
-    /* Return a Comma Separation List of the Attributes */
-    public String toString() {
-        return "%s,%s,%s,%s,%s".formatted(
-            accountNumber,
-            sortCode,
-            balance,
-            maxOverdraft,
-            userType
-        );
-    };
-
-    /* Add an Amount to the Balance */
-    public boolean makeDeposit(int amount) {
-        balance = balance + amount;
-        return true;
-    };
-
-    /* Subtract an Amount from the Balance */
-    public boolean makeWithdrawal(int amount) {
-        balance = balance - amount;
-
-        /* Compare against the MaxOverdraft Limit */
-        if (balance < maxOverdraft) {
-            System.out.println("You cannot withdraw £%s and go past the £%s overdraft...".formatted(
-                amount, maxOverdraft
-            ));
-            balance = balance + amount;
-            return false;
-        };
-
-        return true;
-    };
-
-    /* Fetch the Value of the Balance */
-    public int getBalance() {
-        return balance;
-    };
-    /* Set the Value of the Balance */
-    public void setBalance(int amount) {
-        balance = amount;
-    };
-};
+		sortCode = "";
+		while (sortCode.matches("\\d{6}") == false)
+		{
+			System.out.print("Enter SortCode: ");
+			sortCode = inputScanner.nextLine();
+		}
+		
+		inputScanner.close();
+		
+		balance = 0;
+		maxOverdraft = -1000;
+		userType = 'C';
+	}
+	
+	
+	/**
+	 * This method will return a string value, of each
+	 * global variable concatenated with a comma seperator.
+	 */
+	public String toString()
+	{
+		return "%s,%s,%s,%s,%s".formatted(
+			accountNumber,
+			sortCode,
+			balance,
+			maxOverdraft,
+			userType
+		);
+	}
+}
